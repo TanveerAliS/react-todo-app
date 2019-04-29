7d8afa1a87e84d23a92dfd844fde4258610f45a0
Tanveer <tanveeralims@gmail.com>
2019-04-29 22:42:44 +0530

    initial commit

---
+++ build/precache-manifest.3a344729ce839348815f6c105447a8e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7365f66dacc9ec3dc60893a4b57af26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a5d8444b2ff5b8eb56",
+    "url": "/static/css/2.e41d57f2.chunk.css"
+  },
+  {
+    "revision": "6759544306d03d08cfea",
+    "url": "/static/css/main.6761f2e8.chunk.css"
+  },
+  {
+    "revision": "93a5d8444b2ff5b8eb56",
+    "url": "/static/js/2.8cb700e7.chunk.js"
+  },
+  {
+    "revision": "6759544306d03d08cfea",
+    "url": "/static/js/main.b7b79767.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/static/media/Roboto-Regular.3e1af3ef.ttf"
+  }
+]);
